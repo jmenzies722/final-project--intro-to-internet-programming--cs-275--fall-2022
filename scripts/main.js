48b43edfb35c950b219860821bec71146e8a0db3
jmenzies722 <101149635+jmenzies722@users.noreply.github.com>
2022-12-10 22:17:07 -0500

    updating

--- scripts/main.js
+++ scripts/main.js
@@ -8,27 +8,27 @@ window.onload = () => {
     const body = document.querySelector(`body`); //selecting the body
     const menu = document.querySelector(`nav`); // selecting the nav
 
-     // shows the  menu or dismiss the menu when toggled
-     const showMenu = document.getElementById(`menu`);
-     showMenu.addEventListener(`click`, () => {
-         menu.classList.toggle(`hidden`);
-     });
+    // shows the  menu or dismiss the menu when toggled
+    const showMenu = document.getElementById(`menu`); //selecting the menu
+    showMenu.addEventListener(`click`, () => {
+        menu.classList.toggle(`hidden`);
+    });
 
-      // when the modal option is clicked that will indicate the modal is on
-    const modal_On = document.getElementById(`modal`);
+    // when the modal option is clicked that will indicate the modal is on
+    const modal_On = document.getElementById(`modal`); //selecting the modal
     modal_On.addEventListener(`click`, () => {
         modal_Off.classList.toggle(`hidden`);
         body.classList.toggle(`no-scroll`);
     });
 
-      // when model is on the, user will be allowed to  click anywhere to dismiss the modal
-      const modal_Off = document.querySelectorAll(`div`)[0];
-      modal_Off.addEventListener(`click`, () => {
-          modal_Off.classList.toggle(`hidden`);
-          body.classList.toggle(`no-scroll`);
-      });
+    // when model is on the, user will be allowed to  click anywhere to dismiss the modal
+    const modal_Off = document.querySelectorAll(`div`)[0];
+    modal_Off.addEventListener(`click`, () => {
+        modal_Off.classList.toggle(`hidden`);
+        body.classList.toggle(`no-scroll`);
+    });
 
-        // an alternative to dismiss the modal will be to use the esc key on the keyboard
+    // an alternative to dismiss the modal will be to use the esc key on the keyboard
     document.addEventListener(`keydown`, (e) => {
         if (e.key === `Escape` && !modal_Off.classList.contains(`hidden`)) {
             modal_Off.click();
