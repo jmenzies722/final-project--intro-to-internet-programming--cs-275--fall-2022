53adcaa42f4c7d0e61ffc02ee48094afbd66ee31
jmenzies722 <101149635+jmenzies722@users.noreply.github.com>
2022-12-12 04:08:51 -0500

    changes in js

--- scripts/main.js
+++ scripts/main.js
@@ -5,24 +5,24 @@ Final Project
 
 //off to start
 window.onload = () => {
-    const body = document.querySelector(`body`); //selecting the body
-    const menu = document.querySelector(`nav`); // selecting the nav
+    let body = document.querySelector(`body`); //selecting the body
+    let menu = document.querySelector(`nav`); // selecting the nav
 
     // shows the  menu or dismiss the menu when toggled
-    const showMenu = document.getElementById(`menu`); //selecting the menu
+    let showMenu = document.getElementById(`menu`); //selecting the menu
     showMenu.addEventListener(`click`, () => {
         menu.classList.toggle(`hidden`);
     });
 
     // when the modal option is clicked that will indicate the modal is on
-    const modal_On = document.getElementById(`modal`); //selecting the modal
+    let modal_On = document.getElementById(`modal`); //selecting the modal
     modal_On.addEventListener(`click`, () => {
         modal_Off.classList.toggle(`hidden`);
         body.classList.toggle(`no-scroll`);
     });
 
     // when model is on the, user will be allowed to click anywhere to dismiss the modal
-    const modal_Off = document.querySelectorAll(`div`)[0];
+    let modal_Off = document.querySelectorAll(`div`)[0];
     modal_Off.addEventListener(`click`, () => {
         modal_Off.classList.toggle(`hidden`);
         body.classList.toggle(`no-scroll`);
